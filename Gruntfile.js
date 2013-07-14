7b2d60d087b4b7485d3e17682d36759aec840e47
Clément Bourgeois <moonpyk@gmail.com>
2013-07-14 23:24:36 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,10 @@
 /*global module:false*/
 module.exports = function (grunt) {
-
     // Project configuration.
     grunt.initConfig({
         // Task configuration.
         sass: {
-            dist: {
+            dev: {
                 files: {
                     'Site.css': 'Site.scss'
                 },
@@ -16,30 +15,42 @@ module.exports = function (grunt) {
             }
         },
         recess: {
-            dist: {
+            dev: {
                 options: {
                     compile: true
                 },
                 files: {
                     'Site.css': 'Site.css'
                 }
+            },
+            dist: {
+                options: {
+                    compile: true,
+                    compress: true
+                },
+                files: {
+                    'Site.min.css': 'Site.css'
+                }
             }
         },
         watch: {
             sass: {
                 files: ['./*.scss', './**/*.scss'],
-                tasks: ['resass']
+                tasks: ['resass:dev']
             }
         }
     });
 
-    grunt.registerTask('resass', ['sass', 'recess']);
+    grunt.registerTask('resass', ['sass:dev', 'recess:dev']);
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-recess');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
 
+    // Dist
+    grunt.registerTask('dist', ['resass', 'recess:dist']);
+
     // Default task.
-    grunt.registerTask('default', [ 'watch']);
+    grunt.registerTask('default', ['resass', 'watch']);
 };
