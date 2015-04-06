0b9292731282b979b8465637c22925a36841ffce
Clément Bourgeois <moonpyk@gmail.com>
2015-04-06 21:03:57 +0200

    New version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,52 @@
 /*global module:false*/
+
 module.exports = function (grunt) {
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    var autoprefixer = require('autoprefixer-core');
+
     // Project configuration.
     grunt.initConfig({
         // Task configuration.
         cssRoot: '.',
         sass: {
-            dev: {
-                files: {
-                    '<%= cssRoot %>/Site.css': '<%= cssRoot %>/Site.scss'
-                },
-                options: {
-                    style: 'expanded ', // compact, compressed, nested or expanded
-                    compass: ''
-                }
+            files: {
+                expand: true,
+                src: ['<%= cssRoot %>/*.scss', '<%= cssRoot %>/!_*.scss'],
+                ext: '.css'
+            },
+            options: {
+                //style: 'compact ', // compact, compressed, nested or expanded
+                precision: 10,
+                compass: ''
             }
         },
-        recess: {
-            dev: {
-                options: {
-                    compile: true
-                },
-                files: {
-                    '<%= cssRoot %>/Site.css': '<%= cssRoot %>/Site.css'
-                }
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    autoprefixer({browsers: ['last 2 version']}).postcss
+                ]
             },
-            dist: {
-                options: {
-                    compile: true,
-                    compress: true
-                },
-                files: {
-                    '<%= cssRoot %>/Site.min.css': '<%= cssRoot %>/Site.css'
-                }
+            files: {
+                expand: true,
+                src: ['<%= cssRoot %>/*.css'],
+                ext: '.css'
             }
         },
         watch: {
             sass: {
                 files: ['<%= cssRoot %>/*.scss', '<%= cssRoot %>/**/*.scss'],
-                tasks: ['resass:dev']
+                tasks: ['sass', 'postcss']
             }
         }
     });
 
-    grunt.registerTask('resass', ['sass:dev', 'recess:dev']);
-
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-recess');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-
-    // Dist
-    grunt.registerTask('dist', ['resass', 'recess:dist']);
-
     // Default task.
-    grunt.registerTask('default', ['resass', 'watch']);
+    grunt.registerTask('default', [
+        'sass', 'postcss'
+    ]);
 };
